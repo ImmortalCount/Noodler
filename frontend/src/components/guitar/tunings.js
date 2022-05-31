550e52b5f8bffaa42dcf202ef6392dfb67e891e3
ImmortalCount <devnaut@gmail.com>
2022-05-31 11:15:30 -0700

    tunings updated

--- frontend/src/components/guitar/tunings.js
+++ frontend/src/components/guitar/tunings.js
@@ -14,13 +14,32 @@ export const tuningOptions = [
     {key: 'DropD', text: 'DropD', value: ['E4', 'B4', 'G3', 'D3', 'A2', 'D2']},
 ]
 
+export const tuningKey = {
+    'E': ['E4'],
+    'BE': ['E4', 'B3'],
+    'GBE': ['E4', 'B3', 'G3'], 
+    'DGBE': ['E4', 'B3', 'G3', 'D3'],
+    'ADGBE': ['E4', 'B3', 'G3', 'D3', 'A2'],
+    'EADGBE': ['E4', 'B3', 'G3', 'D3', 'A2', 'E2'],
+    'BEADGBE': ['E4', 'B3', 'G3', 'D3', 'A2', 'E2', 'B1'],
+    'F#BEADGBE': ['E4', 'B3', 'G3', 'D3', 'A2', 'E2', 'B1', 'F#1'],
+    'C#F#BEADGBE': ['E4', 'B3', 'G3', 'D3', 'A2', 'E2', 'B1', 'F#1', 'C#1'],
+    'Bass: EADG': ['G2', 'D2', 'A1', 'E1'],
+    'Bass: BEADG': ['G2', 'D2', 'A1', 'E1', 'B0'],
+    'Bass: F#BEADG': ['G2', 'D2', 'A1', 'E1', 'B0', 'F#0'],
+    'Bass: Drop D': ['G2', 'D2', 'A1', 'D1'],
+}
+
 export const tuningOptionsGuitar = [
     {key: 'Standard 1', text: 'E', value: ['E4']},
     {key: 'Standard 2', text: 'BE', value: ['E4', 'B3']},
     {key: 'Standard 3', text: 'GBE', value: ['E4', 'B3', 'G3']},
     {key: 'Standard 4', text: 'DGBE', value: ['E4', 'B3', 'G3', 'D3']},
     {key: 'Standard 5', text: 'ADGBE', value: ['E4', 'B3', 'G3', 'D3', 'A2']},
+    {key: 'Standard 6', text: 'EADGCF', value: ['F4', 'C4', 'G3', 'D3', 'A2', 'E2']},
     {key: 'Standard 6', text: 'EADGBE', value: ['E4', 'B3', 'G3', 'D3', 'A2', 'E2']},
+    {key: 'Standard 6', text: 'DADGBE', value: ['E4', 'B3', 'G3', 'D3', 'A2', 'D2']},
+    {key: 'Standard 6', text: 'CGCFAD', value: ['D4', 'A3', 'F3', 'C3', 'G2', 'C2']},
     {key: 'Standard 7', text: 'BEADGBE', value: ['E4', 'B3', 'G3', 'D3', 'A2', 'E2', 'B1']},
     {key: 'Standard 8', text: 'F#BEADGBE', value: ['E4', 'B3', 'G3', 'D3', 'A2', 'E2', 'B1', 'F#1']},
     {key: 'Standard 9', text: 'C#F#BEADGBE', value: ['E4', 'B3', 'G3', 'D3', 'A2', 'E2', 'B1', 'F#1', 'C#1']},
