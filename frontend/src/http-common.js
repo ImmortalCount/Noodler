0d1486c959ee1a51176ef5dae896509f6ccaa001
ImmortalCount <devnaut@gmail.com>
2022-06-01 17:39:30 -0700

    changed for production

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 //prod "https://noodler-backend.herokuapp.com/api/"
 //local "http://localhost:5000/api/"
 export default axios.create({
-    baseURL: "http://localhost:5000/api/",
+    baseURL: "https://noodler-backend.herokuapp.com/api/",
     headers: {
         "Content-type": "application/json"
     }
