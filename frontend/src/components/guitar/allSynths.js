2537b79a9eebc89392bea736ffca1b94031c4613
ImmortalCount <devnaut@gmail.com>
2022-06-02 09:26:17 -0700

    synth update

--- frontend/src/components/guitar/allSynths.js
+++ frontend/src/components/guitar/allSynths.js
@@ -14,18 +14,18 @@ const possibleSynths = {
 
 //synths in use
 export const currentSynths = {
-    // 'acoustic_bass': new Tone.Sampler(instrumentSamples.acoustic_bass).toDestination(),
-    // 'acoustic_bass_2': new Tone.Sampler(instrumentSamples.acoustic_bass).toDestination(),
-    // 'electric_bass_finger': new Tone.Sampler(instrumentSamples.electric_bass_finger).toDestination(),
-    // 'electric_bass_finger_2': new Tone.Sampler(instrumentSamples.electric_bass_finger).toDestination(),
+    'acoustic_bass': new Tone.Sampler(instrumentSamples.acoustic_bass).toDestination(),
+    'acoustic_bass_2': new Tone.Sampler(instrumentSamples.acoustic_bass).toDestination(),
+    'electric_bass_finger': new Tone.Sampler(instrumentSamples.electric_bass_finger).toDestination(),
+    'electric_bass_finger_2': new Tone.Sampler(instrumentSamples.electric_bass_finger).toDestination(),
     'acoustic_guitar_nylon': new Tone.Sampler(instrumentSamples.acoustic_guitar_nylon).toDestination(),
-    // 'acoustic_guitar_nylon_2': new Tone.Sampler(instrumentSamples.acoustic_guitar_nylon).toDestination(),
-    // 'acoustic_guitar_steel': new Tone.Sampler(instrumentSamples.acoustic_guitar_steel).toDestination(),
-    // 'acoustic_guitar_steel_2': new Tone.Sampler(instrumentSamples.acoustic_guitar_steel).toDestination(),
-    // 'electric_guitar_clean': new Tone.Sampler(instrumentSamples.electric_guitar_clean).toDestination(),
-    // 'electric_guitar_clean_2': new Tone.Sampler(instrumentSamples.electric_guitar_clean).toDestination(),
-    // 'electric_guitar_jazz': new Tone.Sampler(instrumentSamples.electric_guitar_jazz).toDestination(),
-    // 'electric_guitar_jazz_2': new Tone.Sampler(instrumentSamples.electric_guitar_jazz).toDestination(),
+    'acoustic_guitar_nylon_2': new Tone.Sampler(instrumentSamples.acoustic_guitar_nylon).toDestination(),
+    'acoustic_guitar_steel': new Tone.Sampler(instrumentSamples.acoustic_guitar_steel).toDestination(),
+    'acoustic_guitar_steel_2': new Tone.Sampler(instrumentSamples.acoustic_guitar_steel).toDestination(),
+    'electric_guitar_clean': new Tone.Sampler(instrumentSamples.electric_guitar_clean).toDestination(),
+    'electric_guitar_clean_2': new Tone.Sampler(instrumentSamples.electric_guitar_clean).toDestination(),
+    'electric_guitar_jazz': new Tone.Sampler(instrumentSamples.electric_guitar_jazz).toDestination(),
+    'electric_guitar_jazz_2': new Tone.Sampler(instrumentSamples.electric_guitar_jazz).toDestination(),
 }
 
 export function nameOfNewSynthSource(newSynthRequest, instruments){
