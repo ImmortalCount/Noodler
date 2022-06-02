3112336d26133b0de6f232d9106d23a8472cb002
ImmortalCount <devnaut@gmail.com>
2022-06-02 00:24:55 -0700

    synth test

--- frontend/src/components/guitar/allSynths.js
+++ frontend/src/components/guitar/allSynths.js
@@ -18,7 +18,7 @@ export const currentSynths = {
     // 'acoustic_bass_2': new Tone.Sampler(instrumentSamples.acoustic_bass).toDestination(),
     // 'electric_bass_finger': new Tone.Sampler(instrumentSamples.electric_bass_finger).toDestination(),
     // 'electric_bass_finger_2': new Tone.Sampler(instrumentSamples.electric_bass_finger).toDestination(),
-    'acoustic_guitar_nylon': new Tone.Sampler(instrumentSamples.acoustic_guitar_nylon).toDestination(),
+    // 'acoustic_guitar_nylon': new Tone.Sampler(instrumentSamples.acoustic_guitar_nylon).toDestination(),
     'acoustic_guitar_nylon_2': new Tone.Sampler(instrumentSamples.acoustic_guitar_nylon).toDestination(),
     // 'acoustic_guitar_steel': new Tone.Sampler(instrumentSamples.acoustic_guitar_steel).toDestination(),
     // 'acoustic_guitar_steel_2': new Tone.Sampler(instrumentSamples.acoustic_guitar_steel).toDestination(),
